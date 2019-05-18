4a6766b709c72a39f55af3ed77334435d8ef505a
Devin Arnold <dbarnold321@gmail.com>
2019-05-18 14:41:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
       :four_pieces_owned => 200
-      
+
       }}}
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
